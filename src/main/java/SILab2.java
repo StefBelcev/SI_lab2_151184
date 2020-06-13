af30c1b23b05ec184ef83c9671bcf5f207e84bb9
StefBelcev <37218669+StefBelcev@users.noreply.github.com>
2020-06-13 23:38:51 +0200

    Update SILab2.java

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -7,41 +7,40 @@ import java.util.List;
 
 public class SILab2 {
     public static List<String> function(List<String> list) {
-        if (list.size() <= 0) {
-            throw new IllegalArgumentException("List length should be greater than 0");
+        if (list.size() <= 0) { //B.1
+            throw new IllegalArgumentException("List length should be greater than 0"); //B.2
         }
-        List<String> numMines = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            if (!list.get(i).equals("#")) {
-                int num = 0;
-                if (i - 1 >= 0 && list.get(i - 1).equals("#")) {
-                    num++;
+        List<String> numMines = new ArrayList<>(); //B.3
+        for (int i = 0; i < list.size(); i++) { //B.4
+            if (!list.get(i).equals("#")) { //B.4.1
+                int num = 0; //B.4.1.1
+                if (i - 1 >= 0 && list.get(i - 1).equals("#")) { //B.4.1.2
+                    num++; //B.4.1.3
                 }
-                if (i + 1 < list.size() && list.get(i + 1).equals("#")) {
-                    num++;
+                if (i + 1 < list.size() && list.get(i + 1).equals("#")) { //B.4.1.4
+                    num++; //B.4.1.3
                 }
-                numMines.add(String.valueOf(num));
-            } else {
-                numMines.add(list.get(i));
+                numMines.add(String.valueOf(num)); //B.4.1.5
+            } else {// B.4.2
+                numMines.add(list.get(i)); //B.4.2.1
             }
         }
-        return numMines;
+        return numMines; //B.5
     }
 }
 class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] minesweeper_array = input.split(" ");
-        List<String> minesweeper_list = new ArrayList<String>();
-        minesweeper_list = Arrays.asList(minesweeper_array);
+        String input = scanner.nextLine(); //A
+        String[] minesweeper_array = input.split(" "); //A
+        List<String> minesweeper_list = Arrays.asList(minesweeper_array); //A
 
-        List<String> minesweeper_transformed;
-        minesweeper_transformed = SILab2.function(minesweeper_list);
-        String output_list = "";
-        for (String m: minesweeper_transformed){
-            output_list += m + " ";
+        List<String> minesweeper_transformed; //A
+        minesweeper_transformed = SILab2.function(minesweeper_list); //B
+        String output_list = ""; //C
+        for (String m: minesweeper_transformed){ //D
+            output_list += m + " "; // E
         }
-        System.out.println(output_list);
+        System.out.println(output_list); // F
     }
 }
